735701a35ca9597d33efa88b5f8781ba642f6f70
Rohid <rohidul209@gmail.com>
2023-04-18 20:06:36 +0600

    content and image opt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true,
   },
+  image: {
+    service: "astro/assets/services/sharp",
+  },
   integrations: [
     tailwind(),
     mdx({
       drafts: false,
-    }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,19 @@
 import { z, defineCollection } from "astro:content";
 
 const postsCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    coverImage: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    publishDate: z.date(),
-    isDraft: z.boolean().default(true),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      coverImage: image()
+        .refine((img) => img.width >= 1080, {
+          message: "Cover image must be at least 1080 pixels wide!",
+        })
+        .optional(),
+      tags: z.array(z.string()).optional(),
+      publishDate: z.date(),
+      isDraft: z.boolean().default(true),
+    }),
 });
 
 const pagesCollection = defineCollection({
@@ -20,20 +25,23 @@ const pagesCollection = defineCollection({
 });
 
 const projectsCollection = defineCollection({
-  schema: z.object({
-    name: z.string(),
-    description: z.string(),
-    coverImage: z.string(),
-    appIcon: z.string().optional(),
-    isLaunched: z.boolean().default(false),
-    urls: z
-      .object({
-        appStore: z.string().optional(),
-        playStore: z.string().optional(),
-        web: z.string().optional(),
-      })
-      .optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      description: z.string(),
+      coverImage: image().refine((img) => img.width >= 1080, {
+        message: "Cover image must be at least 1080 pixels wide!",
+      }),
+      appIcon: z.string().optional(),
+      isLaunched: z.boolean().default(false),
+      urls: z
+        .object({
+          appStore: z.string().optional(),
+          playStore: z.string().optional(),
+          web: z.string().optional(),
+        })
+        .optional(),
+    }),
 });
 
 export const collections = {
