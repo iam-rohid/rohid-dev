eae511cc20485063cfb151ddf6e5057e5eb231c0
Rohid <rohidul209@gmail.com>
2023-04-18 21:01:28 +0600

    vercel/static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,22 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true
+    assets: true,
   },
   image: {
-    service: "astro/assets/services/sharp"
+    service: "astro/assets/services/sharp",
   },
-  integrations: [tailwind(), mdx({
-    drafts: false
-  })],
-  output: "server",
-  adapter: vercel()
+  integrations: [
+    tailwind(),
+    mdx({
+      drafts: false,
+    }),
+  ],
+  output: "static",
+  adapter: vercel(),
 });
