0ae90c3743f291500abc7ca071f72897ce52a30c
Rohid <rohidul209@gmail.com>
2023-07-06 17:40:14 +0600

    react added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import vercel from "@astrojs/vercel/static";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   image: {
-    service: "astro/assets/services/sharp",
+    service: "astro/assets/services/sharp"
   },
-  integrations: [
-    tailwind(),
-    mdx({
-      drafts: false,
-    }),
-  ],
+  integrations: [tailwind(), mdx({
+    drafts: false
+  }), react()],
   output: "static",
-  adapter: vercel(),
+  adapter: vercel()
 });
