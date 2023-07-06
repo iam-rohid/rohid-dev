6cd6d37ed8ef8338988e887604cd3d9f522a670e
Rohid <rohidul209@gmail.com>
2023-07-06 18:09:46 +0600

    keystatic setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true
+    assets: true,
   },
-  image: {
-    service: "astro/assets/services/sharp"
-  },
-  integrations: [tailwind(), mdx({
-    drafts: false
-  }), react()],
-  output: "static",
-  adapter: vercel()
+  integrations: [
+    tailwind(),
+    mdx({
+      drafts: false,
+    }),
+    react(),
+  ],
+  output: "hybrid",
+  adapter: vercel(),
 });
