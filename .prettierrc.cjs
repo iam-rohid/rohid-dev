ef95bead3d3c90786a5666a1259dd985166cd8ef
Rohid <rohidul209@gmail.com>
2023-08-03 23:27:09 +0600

    pritter with tailwind plugin

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [require.resolve('prettier-plugin-astro'), require.resolve("prettier-plugin-tailwindcss")],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  astroAllowShorthand: false
+};
