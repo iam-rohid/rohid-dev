c454c6e2a3c4428a7ee43a391d656d7e96f5486f
Rohid <rohidul209@gmail.com>
2023-11-01 16:59:57 +0600

    base url prefix fix

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,3 @@
-export const BASE_URL =
-  `https://${process.env.PUBLIC_VERCEL_URL}` ?? "http://localhost:3000";
+export const BASE_URL = process.env.PUBLIC_VERCEL_URL
+  ? `https://${process.env.PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
